f8564dcc04c7877ddf95bfac5b4b0c60191e1477
Jackie Yeh <q0821yeh@gmail.com>
2025-04-01 02:53:53 +0800

    dotfiles auto backup: 2025-04-01 02:53:53

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/dotfiles || exit 1
+
+# 記錄當前時間
+NOW=$(date "+%Y-%m-%d %H:%M:%S")
+
+# Git 操作
+git add .
+git commit -m "dotfiles auto backup: $NOW" >/dev/null 2>&1 || echo "⚠️ No changes to commit"
+git push origin master
