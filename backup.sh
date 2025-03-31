c36846a658f6aeac51963815dd23950f2b2970ab
Jackie Yeh <q0821yeh@gmail.com>
2025-04-01 02:55:07 +0800

    dotfiles auto backup: 2025-04-01 02:55:07

--- backup.sh
+++ backup.sh
@@ -8,4 +8,5 @@ NOW=$(date "+%Y-%m-%d %H:%M:%S")
 # Git 操作
 git add .
 git commit -m "dotfiles auto backup: $NOW" >/dev/null 2>&1 || echo "⚠️ No changes to commit"
-git push origin main
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git push origin "$CURRENT_BRANCH"
